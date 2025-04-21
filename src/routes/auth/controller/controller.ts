c2500ed8ba7f162b8d73cad150806eba0d2eec8c
heonq <gkagjsrb@naver.com>
2025-04-21 17:19:06 +0900

    feat: cookie domain 설정

--- src/routes/auth/controller/controller.ts
+++ src/routes/auth/controller/controller.ts
@@ -7,6 +7,10 @@ export class AuthController {
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? ('none' as const) : ('lax' as const),
     path: '/',
+    domain:
+      process.env.NODE_ENV === 'production'
+        ? process.env.DEPLOYED_URL!.replace(/https?:\/\//, '')
+        : 'localhost',
   };
   constructor(private authService: AuthService) {}
 
